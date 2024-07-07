2bb8b3158fec635e0a946d0a313d7686ef88b885
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-07-07 18:03:02 +0530

    ✌🏼using-second-overloaded-version-of-getBean(Class<T>)-method🛠️

--- module-01-spring-core/spring-core-app-01-XmlBeanFactory/src/io/spring/app/UseStudent.java
+++ module-01-spring-core/spring-core-app-01-XmlBeanFactory/src/io/spring/app/UseStudent.java
@@ -11,12 +11,20 @@ public class UseStudent {
         Resource resourceFile = new ClassPathResource("/io/spring/resources/beanconf.xml");
         BeanFactory container = new XmlBeanFactory(resourceFile);
 
-        // NOTE: BeanFactory performs Lazy initialization
+        // 📌 NOTE: BeanFactory performs Lazy initialization
         System.out.println("Container Started");
 
-        // Getting Student Object from Spring Container
-        Student studentObj = (Student) container.getBean("studentObj");
+        // 👇🏼 Fetching Student Object from the Spring Container
 
+        // Version 1: Using .getBean(String) method
+        // This method requires providing the bean name as a string argument and explicit type casting.
+        // Student studentObj = (Student) container.getBean("studentObj");
+
+        // Version 2: Using .getBean(Class<T>) method
+        // This method requires the class type as an argument and eliminates the need for explicit type casting.
+        Student studentObj = container.getBean(Student.class);
+
+        // Printing out the details of the retrieved Student object
         System.out.println("Student name: " + studentObj.getName());
         System.out.println("Student Roll no: " + studentObj.getRollNo());
     }
