3b651a25162a48fd4d1994d3045f6affd8db092c
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-07-08 20:59:21 +0530

    </>programmer-bean👨‍💻👩🏻‍💻

---
+++ module-01-spring-core/spring-core-app-03-creating-bean-objects-of-different-classes/src/io/spring/beans/Programmer.java
@@ -0,0 +1,46 @@
+package io.spring.beans;
+
+public class Programmer {
+    private String name;
+    private int age;
+    private String language;
+    private Laptop laptop;
+
+    // Constructor
+    public Programmer() {
+        System.out.println("Programmer Bean Created");
+    }
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public Laptop getLaptop() {
+        return laptop;
+    }
+
+    public void setLaptop(Laptop laptop) {
+        this.laptop = laptop;
+    }
+}
