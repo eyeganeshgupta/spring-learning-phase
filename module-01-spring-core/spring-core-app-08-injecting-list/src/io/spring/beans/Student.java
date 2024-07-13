701fcdc506438a3815179389cc11d0d9db926724
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-07-13 16:06:19 +0530

    👨🏻‍🎓student-bean

---
+++ module-01-spring-core/spring-core-app-08-injecting-list/src/io/spring/beans/Student.java
@@ -0,0 +1,83 @@
+package io.spring.beans;
+
+import java.util.List;
+
+public class Student {
+    private String studentId;
+    private String name;
+    private int age;
+    private String grade;
+    private List<String> subjects;
+    private List<Integer> marks;
+
+    // Constructor
+    public Student() {
+        System.out.println("Student Bean Created!");
+    }
+
+    // Getter and Setter for studentId
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    // Getter and Setter for name
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter and Setter for age
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    // Getter and Setter for grade
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    // Getter and Setter for subjects
+    public List<String> getSubjects() {
+        return subjects;
+    }
+
+    public void setSubjects(List<String> subjects) {
+        this.subjects = subjects;
+    }
+
+    // Getter and Setter for marks
+    public List<Integer> getMarks() {
+        return marks;
+    }
+
+    public void setMarks(List<Integer> marks) {
+        this.marks = marks;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "studentId='" + studentId + '\'' +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", grade='" + grade + '\'' +
+                ", subjects=" + subjects +
+                ", marks=" + marks +
+                '}';
+    }
+}
