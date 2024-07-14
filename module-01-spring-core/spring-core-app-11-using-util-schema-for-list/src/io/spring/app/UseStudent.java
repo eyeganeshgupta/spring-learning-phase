926ae684dfbe6d9517f4f1acc93e90d389732c94
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-07-14 23:12:21 +0530

    👨‍💻working-with-util-schema-for-list

---
+++ module-01-spring-core/spring-core-app-11-using-util-schema-for-list/src/io/spring/app/UseStudent.java
@@ -0,0 +1,25 @@
+package io.spring.app;
+
+import io.spring.beans.Student;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class UseStudent {
+    public static void main(String[] args) {
+        ApplicationContext container = new ClassPathXmlApplicationContext("/io/spring/resources/config.xml");
+
+        // 👇🏼 Fetching Student Object from the Spring Container
+        // Using .getBean(String, Class) method
+        Student studentObj = container.getBean("studentObj", Student.class);
+
+        // 📇 Printing out the details of the retrieved studentObj
+        System.out.println("\nStudent id: " + studentObj.getStudentId());
+        System.out.println("Name: " + studentObj.getName());
+        System.out.println("Age: " + studentObj.getAge());
+        System.out.println("Grade: " + studentObj.getGrade());
+        System.out.println("\nSubjects: ");
+        studentObj.getSubjects().forEach(subject -> System.out.println(subject));
+        System.out.println("\nMarks Obtained: ");
+        studentObj.getMarks().forEach(mark -> System.out.println(mark));
+    }
+}
