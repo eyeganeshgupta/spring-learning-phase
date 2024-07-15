a118eb662c027c3354e627054617efb660c2600f
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-07-15 19:26:43 +0530

    👨🏽‍💻working-with-SetFactoryBean

---
+++ module-01-spring-core/spring-core-app-13-using-SetFactoryBean/src/io/spring/app/UseStudent.java
@@ -0,0 +1,23 @@
+package io.spring.app;
+
+import io.spring.beans.Student;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class UseStudent {
+    public static void main(String[] args) {
+        ApplicationContext container = new ClassPathXmlApplicationContext("/io/spring/resources/config.xml");
+
+        // 👇🏼 Fetching Student Object from the Spring Container
+        // Using .getBean(String, Class) method
+        Student studentObj = container.getBean("studentObj", Student.class);
+
+        // 📇 Printing out the details of the retrieved studentObj
+        System.out.println("\nStudent id: " + studentObj.getStudentId());
+        System.out.println("Name: " + studentObj.getName());
+        System.out.println("Age: " + studentObj.getAge());
+        System.out.println("Grade: " + studentObj.getGrade());
+        System.out.println("\nEmails: ");
+        studentObj.getEmails().forEach(email -> System.out.println(email));
+    }
+}
