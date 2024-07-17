4f001a3e5c65040de6c49ff07168febbe7ecdc9e
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-07-17 17:52:09 +0530

    🙋🏻‍♂️student-bean-class

---
+++ module-01-spring-core/spring-core-app-18-getting-familiar-with-constructor-injection-01/src/io/spring/beans/Student.java
@@ -0,0 +1,20 @@
+package io.spring.beans;
+
+public class Student {
+    private String name;
+
+    // Constructor
+    public Student(String name) {
+        this.name = name;
+    }
+
+    // Getter
+    public String getName() {
+        return name;
+    }
+
+    // Setter
+    public void setName(String name) {
+        this.name = name;
+    }
+}
