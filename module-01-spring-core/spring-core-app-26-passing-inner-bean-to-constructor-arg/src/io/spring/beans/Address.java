160d342bb4c9a24c8762de9be7db7aca896a40d5
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-07-19 22:51:03 +0530

    🗺️Address-bean

---
+++ module-01-spring-core/spring-core-app-26-passing-inner-bean-to-constructor-arg/src/io/spring/beans/Address.java
@@ -0,0 +1,92 @@
+package io.spring.beans;
+
+public class Address {
+    private String houseNumber;
+    private String landmark;
+    private String street;
+    private String city;
+    private String state;
+    private String country;
+    private String zipCode;
+
+    // Constructor
+    public Address() {
+        System.out.println("Address Non-parameterized constructor called!");
+    }
+
+    // Getter and Setter for houseNumber
+    public String getHouseNumber() {
+        return houseNumber;
+    }
+
+    public void setHouseNumber(String houseNumber) {
+        this.houseNumber = houseNumber;
+    }
+
+    // Getter and Setter for street
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    // Getter and Setter for landmark
+    public String getLandmark() {
+        return landmark;
+    }
+
+    public void setLandmark(String landmark) {
+        this.landmark = landmark;
+    }
+
+    // Getter and Setter for city
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    // Getter and Setter for state
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    // Getter and Setter for country
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    // Getter and Setter for zipCode
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "houseNumber='" + houseNumber + '\'' +
+                ", landmark='" + landmark + '\'' +
+                ", street='" + street + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", country='" + country + '\'' +
+                ", zipCode='" + zipCode + '\'' +
+                '}';
+    }
+}
