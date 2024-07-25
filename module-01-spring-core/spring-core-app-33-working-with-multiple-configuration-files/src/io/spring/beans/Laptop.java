f6c6b99f9abc9f225af026d7af803c16ceef0766
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-07-25 22:46:22 +0530

    ✔️beans

---
+++ module-01-spring-core/spring-core-app-33-working-with-multiple-configuration-files/src/io/spring/beans/Laptop.java
@@ -0,0 +1,52 @@
+package io.spring.beans;
+
+public class Laptop implements Computer {
+    private String brand;
+    private String processor;
+    private int ramSize;
+    private int storageSize;
+
+    public Laptop() {
+        System.out.println("Laptop Bean Created");
+    }
+
+    @Override
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    @Override
+    public String getBrand() {
+        return brand;
+    }
+
+    @Override
+    public void setProcessor(String processor) {
+        this.processor = processor;
+    }
+
+    @Override
+    public String getProcessor() {
+        return processor;
+    }
+
+    @Override
+    public void setRamSize(int ramSize) {
+        this.ramSize = ramSize;
+    }
+
+    @Override
+    public int getRamSize() {
+        return ramSize;
+    }
+
+    @Override
+    public void setStorageSize(int storageSize) {
+        this.storageSize = storageSize;
+    }
+
+    @Override
+    public int getStorageSize() {
+        return storageSize;
+    }
+}
