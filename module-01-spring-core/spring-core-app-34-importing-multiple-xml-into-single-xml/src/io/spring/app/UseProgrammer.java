5d0a58d29986cffd9b936a5b4a6dfd7094aa2a53
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-07-26 23:03:39 +0530

    📥importing-multiple-xml-into-single-xml

---
+++ module-01-spring-core/spring-core-app-34-importing-multiple-xml-into-single-xml/src/io/spring/app/UseProgrammer.java
@@ -0,0 +1,19 @@
+package io.spring.app;
+
+import io.spring.beans.Programmer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class UseProgrammer {
+    public static void main(String[] args) {
+        ApplicationContext container = new ClassPathXmlApplicationContext("/io/spring/resources/combined-config.xml");
+
+        // 👇🏼 Fetching Programmer Object from the Spring Container
+        Programmer programmerObj01 = container.getBean("programmerObj01", Programmer.class);
+        Programmer programmerObj02 = container.getBean("programmerObj02", Programmer.class);
+
+        // 📇 Printing out the details of the retrieved programmerObj01 and programmerObj02
+        programmerObj01.displayDetails();
+        programmerObj02.displayDetails();
+    }
+}
