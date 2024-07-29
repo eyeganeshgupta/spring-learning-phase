79b36975d6d101691ebda40f7bca72c0ec15ff36
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-07-29 23:10:37 +0530

    👨🏻‍💻working-with-prototype-scope🔭

---
+++ module-01-spring-core/spring-core-app-38-more-on-prototype-scope/src/io/spring/app/UseProgrammer.java
@@ -0,0 +1,27 @@
+package io.spring.app;
+
+import io.spring.beans.Programmer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class UseProgrammer {
+    public static void main(String[] args) {
+        ApplicationContext container = new ClassPathXmlApplicationContext("/io/spring/resources/config.xml");
+
+        // 👇🏼 Fetching Programmer Object from the Spring Container
+        Programmer programmerObj01 = container.getBean("programmerObj", Programmer.class);
+        Programmer programmerObj02 = container.getBean("programmerObj", Programmer.class);
+
+        // 📇 Printing out the details of the retrieved programmerObj01 and programmerObj02
+        System.out.println("\nBefore Changing Value: ");
+        programmerObj01.displayDetails();
+        programmerObj02.displayDetails();
+
+        programmerObj01.setName("Virat Gupta");
+        programmerObj02.setLanguage("Python");
+
+        System.out.println("\n\nAfter Changing Value: ");
+        programmerObj01.displayDetails();
+        programmerObj02.displayDetails();
+    }
+}
