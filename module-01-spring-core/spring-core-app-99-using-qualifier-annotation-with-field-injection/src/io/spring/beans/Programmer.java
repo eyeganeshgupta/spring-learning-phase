61f417ef448e459c84b81f2b22dcfb8c670dcd5d
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-09-11 18:19:25 +0530

    using-@Qualifier-annotation-with-field-injection

---
+++ module-01-spring-core/spring-core-app-99-using-qualifier-annotation-with-field-injection/src/io/spring/beans/Programmer.java
@@ -0,0 +1,123 @@
+package io.spring.beans;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Programmer {
+    @Value("Ganesh Gupta")
+    private String name;
+    @Value("21")
+    private int age;
+    @Value("Male")
+    private String gender;
+    @Value("1")
+    private int yearsOfExperience;
+    @Value("Full Stack Web Developer")
+    private String position;
+    @Value("400000")
+    private double salary;
+    @Value("true")
+    private boolean remoteWorking;
+    @Autowired
+    @Qualifier("developmentEnvironment02")
+    private DevelopmentEnvironment developmentEnvironment;  // New property
+
+    public Programmer() {
+        System.out.println("==== Programmer bean created! ====");
+    }
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getYearsOfExperience() {
+        return yearsOfExperience;
+    }
+
+    public void setYearsOfExperience(int yearsOfExperience) {
+        this.yearsOfExperience = yearsOfExperience;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public boolean isRemoteWorking() {
+        return remoteWorking;
+    }
+
+    public void setRemoteWorking(boolean remoteWorking) {
+        this.remoteWorking = remoteWorking;
+    }
+
+    public DevelopmentEnvironment getDevelopmentEnvironment() {
+        return developmentEnvironment;
+    }
+
+    public void setDevelopmentEnvironment(DevelopmentEnvironment developmentEnvironment) {
+        this.developmentEnvironment = developmentEnvironment;
+    }
+
+    // Method to display programmer details
+    public void displayProgrammerDetails() {
+        String border = "==========================================";
+        String header = "            Programmer Details            ";
+        String line1 = String.format("%-20s : %s", "Name", name);
+        String line2 = String.format("%-20s : %d", "Age", age);
+        String line3 = String.format("%-20s : %s", "Gender", gender);
+        String line4 = String.format("%-20s : %d years", "Experience", yearsOfExperience);
+        String line5 = String.format("%-20s : %s", "Position", position);
+        String line6 = String.format("%-20s : %.2f", "Salary", salary);
+        String line7 = String.format("%-20s : %s", "Remote Working", remoteWorking ? "Yes" : "No");
+
+        System.out.println("\n" + border);
+        System.out.println(header);
+        System.out.println(border);
+        System.out.println(line1);
+        System.out.println(line2);
+        System.out.println(line3);
+        System.out.println(line4);
+        System.out.println(line5);
+        System.out.println(line6);
+        System.out.println(line7);
+
+        System.out.println("\nDevelopment Environment:");
+        developmentEnvironment.displayEnvironmentDetails();  // Display development environment details
+    }
+}
---
+++ module-01-spring-core/spring-core-app-99-using-qualifier-annotation-with-field-injection/src/io/spring/config/AppConfig.java
@@ -0,0 +1,20 @@
+package io.spring.config;
+
+import io.spring.beans.DevelopmentEnvironment;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "io.spring.beans")
+public class AppConfig {
+    @Bean
+    public DevelopmentEnvironment developmentEnvironment01() {
+        return new DevelopmentEnvironment("Ubuntu", "IntelliJ", "GIT", "Maven", "MySQL", "JUnit", "AWS", "SonarQube", "Log4j", "Vagrant");
+    }
+
+    @Bean
+    public DevelopmentEnvironment developmentEnvironment02() {
+        return new DevelopmentEnvironment("Windows 11", "Eclipse", "SVN", "Gradle", "PostgreSQL", "Selenium", "Heroku", "Checkstyle", "SLF4J", "VirtualBox");
+    }
+}
