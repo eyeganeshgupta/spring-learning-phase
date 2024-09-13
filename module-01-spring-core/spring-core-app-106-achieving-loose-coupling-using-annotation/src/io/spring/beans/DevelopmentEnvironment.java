071ebe4abc03be5384a40419b9b7620328b989a0
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-09-13 09:04:36 +0530

    implementation-of-environment-interface

---
+++ module-01-spring-core/spring-core-app-106-achieving-loose-coupling-using-annotation/src/io/spring/beans/DevelopmentEnvironment.java
@@ -0,0 +1,64 @@
+package io.spring.beans;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
+public class DevelopmentEnvironment implements Environment {
+    private boolean isDebugMode;
+    private String baseURL;
+    private String databaseConnectionString;
+    private String developerToolsVersion;
+    private boolean autoReloadEnabled;
+
+    // Constructor
+    public DevelopmentEnvironment() {
+        System.out.println("==== DevelopmentEnvironment bean created! ====");
+    }
+
+    public DevelopmentEnvironment(@Value("true") boolean isDebugMode, @Value("http://localhost:8080") String baseURL, @Value("jdbc:devdb://localhost:3306/devdb") String databaseConnectionString,
+                                  @Value("v1.2.3") String developerToolsVersion, @Value("true") boolean autoReloadEnabled) {
+        this.isDebugMode = isDebugMode;
+        this.baseURL = baseURL;
+        this.databaseConnectionString = databaseConnectionString;
+        this.developerToolsVersion = developerToolsVersion;
+        this.autoReloadEnabled = autoReloadEnabled;
+        System.out.println("==== DevelopmentEnvironment bean created! ====");
+    }
+
+    @Override
+    public void setup() {
+        System.out.println("Setting up Development Environment...");
+        System.out.println("Debug Mode: " + (isDebugMode ? "Enabled" : "Disabled"));
+        System.out.println("Base URL: " + baseURL);
+        System.out.println("Database Connection: " + databaseConnectionString);
+        System.out.println("Developer Tools Version: " + developerToolsVersion);
+        System.out.println("Auto-Reload: " + (autoReloadEnabled ? "Enabled" : "Disabled"));
+    }
+
+    @Override
+    public String getEnvironmentName() {
+        return "Development Environment";
+    }
+
+    @Override
+    public String getBaseURL() {
+        return baseURL;
+    }
+
+    @Override
+    public String getDatabaseConnectionString() {
+        return databaseConnectionString;
+    }
+
+    // Additional Getters
+    public String getDeveloperToolsVersion() {
+        return developerToolsVersion;
+    }
+
+    public boolean isAutoReloadEnabled() {
+        return autoReloadEnabled;
+    }
+}
---
+++ module-01-spring-core/spring-core-app-106-achieving-loose-coupling-using-annotation/src/io/spring/beans/TestingEnvironment.java
@@ -0,0 +1,66 @@
+package io.spring.beans;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestingEnvironment implements Environment {
+    private double testCoveragePercentage;
+    private String baseURL;
+    private String databaseConnectionString;
+    private String testFrameworkVersion;
+    private int numberOfTestSuites;
+
+    // Constructor
+    public TestingEnvironment() {
+        System.out.println("==== TestingEnvironment bean created! ====");
+    }
+
+    public TestingEnvironment(@Value("85.5") double testCoveragePercentage, @Value("http://localhost:9090") String baseURL, @Value("jdbc:testdb://localhost:3306/testdb") String databaseConnectionString,
+                              @Value("JUnit v5") String testFrameworkVersion, @Value("25") int numberOfTestSuites) {
+        this.testCoveragePercentage = testCoveragePercentage;
+        this.baseURL = baseURL;
+        this.databaseConnectionString = databaseConnectionString;
+        this.testFrameworkVersion = testFrameworkVersion;
+        this.numberOfTestSuites = numberOfTestSuites;
+        System.out.println("==== TestingEnvironment bean created! ====");
+    }
+
+    @Override
+    public void setup() {
+        System.out.println("Setting up Testing Environment...");
+        System.out.println("Test Coverage: " + testCoveragePercentage + "%");
+        System.out.println("Base URL: " + baseURL);
+        System.out.println("Database Connection: " + databaseConnectionString);
+        System.out.println("Test Framework Version: " + testFrameworkVersion);
+        System.out.println("Number of Test Suites: " + numberOfTestSuites);
+    }
+
+    @Override
+    public String getEnvironmentName() {
+        return "Testing Environment";
+    }
+
+    @Override
+    public String getBaseURL() {
+        return baseURL;
+    }
+
+    @Override
+    public String getDatabaseConnectionString() {
+        return databaseConnectionString;
+    }
+
+    // Additional Getters
+    public String getTestFrameworkVersion() {
+        return testFrameworkVersion;
+    }
+
+    public int getNumberOfTestSuites() {
+        return numberOfTestSuites;
+    }
+
+    public double getTestCoveragePercentage() {
+        return testCoveragePercentage;
+    }
+}
