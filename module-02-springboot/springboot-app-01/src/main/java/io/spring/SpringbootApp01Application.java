d04f94f6ff019ccd99710c8ab205c8bd1cca6523
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-09-19 11:34:02 +0530

    first-springboot-app

---
+++ module-02-springboot/springboot-app-01/src/main/java/io/spring/SpringbootApp01Application.java
@@ -0,0 +1,11 @@
+package io.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApp01Application {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApp01Application.class, args);
+    }
+}
