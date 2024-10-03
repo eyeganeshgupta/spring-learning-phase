a46741918234ff26baaae8b77c8a60e748e7ddc8
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-10-03 23:19:25 +0530

    ✔️Added Spring JDBC and H2 Database Dependency

---
+++ module-03-spring-jdbc/spring-jdbc-app-01-working-with-h2-database/src/main/java/io/spring/SpringJdbcApp01WorkingWithH2DatabaseApplication.java
@@ -0,0 +1,13 @@
+package io.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJdbcApp01WorkingWithH2DatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJdbcApp01WorkingWithH2DatabaseApplication.class, args);
+    }
+
+}
