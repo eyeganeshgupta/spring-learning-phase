86842a52c8fd61e10b20b442657b9d12e3b7b73e
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-10-04 23:49:51 +0530

    Student schema and model

---
+++ module-03-spring-jdbc/spring-jdbc-app-02/src/main/resources/schema.sql
@@ -0,0 +1,1 @@
+CREATE TABLE Students (student_id INT PRIMARY KEY, first_name VARCHAR(50), last_name VARCHAR(50), email VARCHAR(100), gender VARCHAR(10), gpa DECIMAL(3,2), credits_earned INT, address VARCHAR(200), city VARCHAR(50), state VARCHAR(50), zip_code VARCHAR(10), country VARCHAR(50), phone_number VARCHAR(20), emergency_contact_phone VARCHAR(20));
