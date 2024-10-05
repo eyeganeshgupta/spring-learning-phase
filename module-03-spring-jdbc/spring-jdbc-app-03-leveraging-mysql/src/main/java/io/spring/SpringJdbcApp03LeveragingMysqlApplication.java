e5fa26be5116615d37b529be7f0c8cd11a0d8383
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-10-05 23:34:23 +0530

    configurations for connecting with mysql

---
+++ module-03-spring-jdbc/spring-jdbc-app-03-leveraging-mysql/src/main/java/io/spring/SpringJdbcApp03LeveragingMysqlApplication.java
@@ -0,0 +1,13 @@
+package io.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJdbcApp03LeveragingMysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJdbcApp03LeveragingMysqlApplication.class, args);
+    }
+
+}
