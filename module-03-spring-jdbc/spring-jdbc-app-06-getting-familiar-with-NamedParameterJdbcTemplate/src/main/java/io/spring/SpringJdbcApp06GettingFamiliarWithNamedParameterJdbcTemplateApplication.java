500d76394885e1887b4e09001260d64a2b4a7f64
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-10-08 23:44:12 +0530

    💻Implement Employee Management System (EMS) for CLI

---
+++ module-03-spring-jdbc/spring-jdbc-app-06-getting-familiar-with-NamedParameterJdbcTemplate/src/main/java/io/spring/SpringJdbcApp06GettingFamiliarWithNamedParameterJdbcTemplateApplication.java
@@ -0,0 +1,13 @@
+package io.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJdbcApp06GettingFamiliarWithNamedParameterJdbcTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJdbcApp06GettingFamiliarWithNamedParameterJdbcTemplateApplication.class, args);
+    }
+
+}
