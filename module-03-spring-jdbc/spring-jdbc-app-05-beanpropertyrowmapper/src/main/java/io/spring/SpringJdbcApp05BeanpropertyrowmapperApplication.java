2acccd43955df8f38d9515790737e7cd24ef1726
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-10-08 20:41:48 +0530

    Implement BeanPropertyRowMapper for improved data mapping

---
+++ module-03-spring-jdbc/spring-jdbc-app-05-beanpropertyrowmapper/src/main/java/io/spring/SpringJdbcApp05BeanpropertyrowmapperApplication.java
@@ -0,0 +1,11 @@
+package io.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJdbcApp05BeanpropertyrowmapperApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJdbcApp05BeanpropertyrowmapperApplication.class, args);
+    }
+}
