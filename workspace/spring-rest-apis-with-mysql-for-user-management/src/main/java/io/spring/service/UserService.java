d542e31d8d296e605d1c600316d974ff69207be7
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-11-03 22:45:31 +0530

    build get all users rest api

--- workspace/spring-rest-apis-with-mysql-for-user-management/src/main/java/io/spring/service/UserService.java
+++ workspace/spring-rest-apis-with-mysql-for-user-management/src/main/java/io/spring/service/UserService.java
@@ -2,7 +2,10 @@ package io.spring.service;
 
 import io.spring.entity.User;
 
+import java.util.List;
+
 public interface UserService {
     User createUser(User user);
     User getUserById(Long id);
+    List<User> getAllUsers();
 }
--- workspace/spring-rest-apis-with-mysql-for-user-management/src/main/java/io/spring/service/impl/UserServiceImpl.java
+++ workspace/spring-rest-apis-with-mysql-for-user-management/src/main/java/io/spring/service/impl/UserServiceImpl.java
@@ -6,6 +6,7 @@ import io.spring.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -26,5 +27,10 @@ public class UserServiceImpl implements UserService {
     public User getUserById(Long id) {
         Optional<User> user = userRepository.findById(id);
         return user.orElse(null);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
     }
 }
