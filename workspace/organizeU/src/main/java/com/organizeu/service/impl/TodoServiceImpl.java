849d2abfb34fb00eacba2ab736cea6f0c0953af7
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-11-09 19:46:32 +0530

    using ModelMapper library

--- workspace/organizeU/src/main/java/com/organizeu/service/impl/TodoServiceImpl.java
+++ workspace/organizeU/src/main/java/com/organizeu/service/impl/TodoServiceImpl.java
@@ -5,31 +5,19 @@ import com.organizeu.entity.Todo;
 import com.organizeu.repository.TodoRepository;
 import com.organizeu.service.TodoService;
 import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class TodoServiceImpl implements TodoService {
     private final TodoRepository todoRepository;
+    private ModelMapper modelMapper;
 
     @Override
     public TodoDTO addTodo(TodoDTO todoDTO) {
-        // convert TodoDTO into Todo JPA Entity
-        Todo todo = new Todo();
-        todo.setTitle(todoDTO.getTitle());
-        todo.setDescription(todoDTO.getDescription());
-        todo.setCompleted(todoDTO.isCompleted());
-
-        // Todo JPA Entity
+        Todo todo = modelMapper.map(todoDTO, Todo.class);
         Todo savedTodo = todoRepository.save(todo);
-
-        // Convert saved Todo JPA entity object into TodoDTO object
-        TodoDTO savedTodoDTO = new TodoDTO();
-        savedTodoDTO.setId(savedTodo.getId());
-        savedTodoDTO.setTitle(savedTodo.getTitle());
-        savedTodoDTO.setDescription(savedTodo.getDescription());
-        savedTodoDTO.setCompleted(savedTodo.isCompleted());
-
-        return savedTodoDTO;
+        return modelMapper.map(savedTodo, TodoDTO.class);
     }
 }
