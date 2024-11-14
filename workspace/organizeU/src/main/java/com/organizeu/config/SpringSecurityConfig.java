97c89ba9785c1dfcf768ed86f7d0bb7b8300e4e5
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-11-14 09:15:50 +0530

    understanding role-based authorization

--- workspace/organizeU/src/main/java/com/organizeu/config/SpringSecurityConfig.java
+++ workspace/organizeU/src/main/java/com/organizeu/config/SpringSecurityConfig.java
@@ -2,6 +2,7 @@ package com.organizeu.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -15,6 +16,11 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SpringSecurityConfig {
 
+    private static final String ROLE_ADMIN = "ADMIN";
+    private static final String ROLE_USER = "USER";
+
+    private static final String API_PATH = "/api/**";
+
     @Bean
     public static PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -23,7 +29,14 @@ public class SpringSecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
-                .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+                .authorizeHttpRequests(authorize -> {
+                    authorize.requestMatchers(HttpMethod.POST, API_PATH).hasRole(ROLE_ADMIN);
+                    authorize.requestMatchers(HttpMethod.PUT, API_PATH).hasRole(ROLE_ADMIN);
+                    authorize.requestMatchers(HttpMethod.PATCH, API_PATH).hasAnyRole(ROLE_ADMIN, ROLE_USER);
+                    authorize.requestMatchers(HttpMethod.DELETE, API_PATH).hasRole(ROLE_ADMIN);
+                    authorize.requestMatchers(HttpMethod.GET, API_PATH).permitAll();
+                    authorize.anyRequest().authenticated();
+                })
                 .httpBasic(Customizer.withDefaults());
         return http.build();
     }
@@ -33,13 +46,13 @@ public class SpringSecurityConfig {
         UserDetails virat = User.builder()
                 .username("virat")
                 .password(passwordEncoder().encode("password"))
-                .roles("USER")
+                .roles(ROLE_USER)
                 .build();
 
         UserDetails admin = User.builder()
                 .username("admin")
                 .password(passwordEncoder().encode("admin"))
-                .roles("ADMIN")
+                .roles(ROLE_ADMIN)
                 .build();
 
         return new InMemoryUserDetailsManager(virat, admin);
