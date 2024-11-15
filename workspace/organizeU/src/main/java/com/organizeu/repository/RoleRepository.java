224fb1b0166785f8fe3936466aac8bc2ff850df4
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-11-15 08:57:52 +0530

    creating CustomUserDetailsService class implements UserDetailsService

--- workspace/organizeU/src/main/java/com/organizeu/repository/RoleRepository.java
+++ workspace/organizeU/src/main/java/com/organizeu/repository/RoleRepository.java
@@ -3,6 +3,6 @@ package com.organizeu.repository;
 import com.organizeu.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RoleRepository  extends JpaRepository<Role, Long> {
+public interface RoleRepository extends JpaRepository<Role, Long> {
 
 }
