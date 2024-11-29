34ad8dadb707f2b8216e7a3a1f9e8b1a0d4ba7d3
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-11-29 22:17:06 +0530

    setting-up development environment for spring data jpa

---
+++ module-05-spring-data-jpa/spring-data-jpa-app-01/src/main/java/io/spring/SpringDataJpaApp01Application.java
@@ -0,0 +1,11 @@
+package io.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaApp01Application {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataJpaApp01Application.class, args);
+    }
+}
