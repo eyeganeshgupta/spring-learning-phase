b566906c2c79ad626d7ef1962e004697ccb2ec9b
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-12-06 09:36:13 +0530

    project setup, entity and dto

---
+++ module-05-spring-data-jpa/spring-data-jpa-app-03-more-on-finder-methods/src/main/java/io/spring/SpringDataJpaApp03FinderMethodsApplication.java
@@ -0,0 +1,11 @@
+package io.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaApp03FinderMethodsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataJpaApp03FinderMethodsApplication.class, args);
+    }
+}
