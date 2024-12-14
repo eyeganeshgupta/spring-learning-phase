ce57d4fc96250db11b6c64ed8d4fdfb5491f5b5c
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-12-14 20:37:07 +0530

    movie entity and dto

---
+++ module-05-spring-data-jpa/spring-data-jpa-app-05-custom-queries-with-projection/src/main/java/io/spring/SpringDataJpaApp05CustomQueriesWithProjectionApplication.java
@@ -0,0 +1,13 @@
+package io.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaApp05CustomQueriesWithProjectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataJpaApp05CustomQueriesWithProjectionApplication.class, args);
+    }
+
+}
