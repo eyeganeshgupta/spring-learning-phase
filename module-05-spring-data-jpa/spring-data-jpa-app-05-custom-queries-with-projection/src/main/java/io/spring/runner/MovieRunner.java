18440bf6a7e492567a525cb9509a15443ca21a7c
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-12-15 11:50:16 +0530

    custom queries with projection

--- module-05-spring-data-jpa/spring-data-jpa-app-05-custom-queries-with-projection/src/main/java/io/spring/runner/MovieRunner.java
+++ module-05-spring-data-jpa/spring-data-jpa-app-05-custom-queries-with-projection/src/main/java/io/spring/runner/MovieRunner.java
@@ -1,5 +1,8 @@
 package io.spring.runner;
 
+import io.spring.repository.BasicMovieInfoProjection;
+import io.spring.repository.FinancialInfoProjection;
+import io.spring.repository.StreamingInfoProjection;
 import io.spring.service.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -16,10 +19,24 @@ public class MovieRunner implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        // Fetching and printing movies with high ratings using projection
         System.out.println("\nMovies with Ratings Above 8.0:");
         movieService.getMoviesWithHighRatings(8.0).forEach(projection ->
                 System.out.println(projection.getTitle() + " - Rating: " + projection.getRating())
         );
+
+        System.out.println("\nBasic Movie Information:");
+        for (BasicMovieInfoProjection info : movieService.getBasicMovieInfo()) {
+            System.out.println("Title: " + info.getTitle() + ", Director: " + info.getDirectorName() + ", Year: " + info.getYearReleased());
+        }
+
+        System.out.println("\nFinancial Information:");
+        for (FinancialInfoProjection financial : movieService.getFinancialInfo()) {
+            System.out.println("Budget: $" + financial.getBudget() + ", Box Office: $" + financial.getBoxOffice());
+        }
+
+        System.out.println("\nStreaming Platforms:");
+        for (StreamingInfoProjection streaming : movieService.getStreamingInfo()) {
+            System.out.println("Title: " + streaming.getTitle() + ", Platforms: " + streaming.getStreamingPlatforms());
+        }
     }
 }
--- module-05-spring-data-jpa/spring-data-jpa-app-05-custom-queries-with-projection/src/main/java/io/spring/service/MovieService.java
+++ module-05-spring-data-jpa/spring-data-jpa-app-05-custom-queries-with-projection/src/main/java/io/spring/service/MovieService.java
@@ -2,12 +2,13 @@ package io.spring.service;
 
 import io.spring.dto.MovieDTO;
 import io.spring.entity.Movie;
-import io.spring.repository.MovieProjection;
-import io.spring.repository.MovieRepository;
+import io.spring.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class MovieService {
@@ -18,13 +19,19 @@ public class MovieService {
         this.movieRepository = movieRepository;
     }
 
-    // Save a new movie
-    public Movie saveMovie(Movie movie) {
-        return movieRepository.save(movie);
-    }
-
-    // Get movies with ratings above a certain value using projection
     public List<MovieProjection> getMoviesWithHighRatings(double minRating) {
         return movieRepository.findMoviesWithRatingAbove(minRating);
+    }
+
+    public List<BasicMovieInfoProjection> getBasicMovieInfo() {
+        return movieRepository.findAllBasicMovieInfo();
+    }
+
+    public List<FinancialInfoProjection> getFinancialInfo() {
+        return movieRepository.findAllFinancialInfo();
+    }
+
+    public List<StreamingInfoProjection> getStreamingInfo() {
+        return movieRepository.findAllStreamingInfo();
     }
 }
