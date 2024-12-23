7684701cc8f5974064372e6211c6f0d4e8b547e5
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-12-23 09:49:56 +0530

    pagination with sorting

---
+++ module-05-spring-data-jpa/spring-data-jpa-app-15-pagination-with-sorting/src/main/java/io/spring/SpringDataJpaApp15PaginationWithSortingApplication.java
@@ -0,0 +1,11 @@
+package io.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaApp15PaginationWithSortingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataJpaApp15PaginationWithSortingApplication.class, args);
+    }
+}
---
+++ module-05-spring-data-jpa/spring-data-jpa-app-15-pagination-with-sorting/src/test/java/io/spring/SpringDataJpaApp15PaginationWithSortingApplicationTests.java
@@ -0,0 +1,13 @@
+package io.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaApp15PaginationWithSortingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
