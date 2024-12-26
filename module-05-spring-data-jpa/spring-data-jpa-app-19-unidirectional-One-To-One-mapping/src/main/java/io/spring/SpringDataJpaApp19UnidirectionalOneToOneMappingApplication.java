e5629dcf5a2c47d61d14a4e16a5765900bd6b242
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-12-26 21:32:18 +0530

    setting up development environment for working with relationships in spring data jpa

---
+++ module-05-spring-data-jpa/spring-data-jpa-app-19-unidirectional-One-To-One-mapping/src/main/java/io/spring/SpringDataJpaApp19UnidirectionalOneToOneMappingApplication.java
@@ -0,0 +1,11 @@
+package io.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaApp19UnidirectionalOneToOneMappingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataJpaApp19UnidirectionalOneToOneMappingApplication.class, args);
+    }
+}
