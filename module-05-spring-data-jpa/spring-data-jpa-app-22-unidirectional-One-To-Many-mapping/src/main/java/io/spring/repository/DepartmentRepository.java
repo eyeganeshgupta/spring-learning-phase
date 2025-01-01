eb55fe46fd79a5590d14d787265b4a94ed32a009
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-01-01 22:40:31 +0530

    repository

---
+++ module-05-spring-data-jpa/spring-data-jpa-app-22-unidirectional-One-To-Many-mapping/src/main/java/io/spring/repository/DepartmentRepository.java
@@ -0,0 +1,8 @@
+package io.spring.repository;
+
+import io.spring.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+
+}
