d8a678145b1f3ba630ef32f4babffb7028b6df91
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-01-12 22:23:48 +0530

    student and course repository

---
+++ module-05-spring-data-jpa/spring-data-jpa-app-24-unidirectional-Many-To-Many-mapping/src/main/java/io/spring/repository/CourseRepository.java
@@ -0,0 +1,10 @@
+package io.spring.repository;
+
+import io.spring.entity.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
+
+}
---
+++ module-05-spring-data-jpa/spring-data-jpa-app-24-unidirectional-Many-To-Many-mapping/src/main/java/io/spring/repository/StudentRepository.java
@@ -0,0 +1,10 @@
+package io.spring.repository;
+
+import io.spring.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+}
