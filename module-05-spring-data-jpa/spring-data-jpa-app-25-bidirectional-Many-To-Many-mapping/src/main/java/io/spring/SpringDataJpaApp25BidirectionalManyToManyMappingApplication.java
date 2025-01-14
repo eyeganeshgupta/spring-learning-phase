cd0a71d7466a99a72d6573ee25d4bf1df4877349
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-01-14 09:59:50 +0530

    setup

---
+++ module-05-spring-data-jpa/spring-data-jpa-app-25-bidirectional-Many-To-Many-mapping/src/main/java/io/spring/SpringDataJpaApp25BidirectionalManyToManyMappingApplication.java
@@ -0,0 +1,11 @@
+package io.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaApp25BidirectionalManyToManyMappingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataJpaApp25BidirectionalManyToManyMappingApplication.class, args);
+    }
+}
