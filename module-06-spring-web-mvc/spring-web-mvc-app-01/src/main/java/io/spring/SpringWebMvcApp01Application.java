af76ead5ea5010037dc12dd52d304d995c0f29db
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-01-19 22:48:37 +0530

    setting up development environment

---
+++ module-06-spring-web-mvc/spring-web-mvc-app-01/src/main/java/io/spring/SpringWebMvcApp01Application.java
@@ -0,0 +1,11 @@
+package io.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebMvcApp01Application {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebMvcApp01Application.class, args);
+    }
+}
