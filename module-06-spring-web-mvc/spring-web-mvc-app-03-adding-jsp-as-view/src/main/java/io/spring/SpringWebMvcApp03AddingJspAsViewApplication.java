560d16a9f49e875a0d48ed07dff4ef1a67147d14
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-01-20 23:08:19 +0530

    development environment configuration required for jsp

---
+++ module-06-spring-web-mvc/spring-web-mvc-app-03-adding-jsp-as-view/src/main/java/io/spring/SpringWebMvcApp03AddingJspAsViewApplication.java
@@ -0,0 +1,11 @@
+package io.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebMvcApp03AddingJspAsViewApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebMvcApp03AddingJspAsViewApplication.class, args);
+    }
+}
