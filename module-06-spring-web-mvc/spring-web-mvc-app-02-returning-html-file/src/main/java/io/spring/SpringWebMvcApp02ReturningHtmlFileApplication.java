2124e5d25cdf5b0466fe29072f462f106cbab076
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-01-20 19:43:30 +0530

    returning html file

---
+++ module-06-spring-web-mvc/spring-web-mvc-app-02-returning-html-file/src/main/java/io/spring/SpringWebMvcApp02ReturningHtmlFileApplication.java
@@ -0,0 +1,11 @@
+package io.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebMvcApp02ReturningHtmlFileApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebMvcApp02ReturningHtmlFileApplication.class, args);
+    }
+}
