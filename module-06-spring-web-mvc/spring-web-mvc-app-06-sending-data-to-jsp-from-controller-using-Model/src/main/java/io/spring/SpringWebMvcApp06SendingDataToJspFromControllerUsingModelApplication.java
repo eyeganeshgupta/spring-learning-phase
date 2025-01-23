a8dbf62013cff23324597720df694a61a2d085e0
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-01-23 19:26:45 +0530

    sending data to jsp from controller using model

---
+++ module-06-spring-web-mvc/spring-web-mvc-app-06-sending-data-to-jsp-from-controller-using-Model/src/main/java/io/spring/SpringWebMvcApp06SendingDataToJspFromControllerUsingModelApplication.java
@@ -0,0 +1,11 @@
+package io.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebMvcApp06SendingDataToJspFromControllerUsingModelApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebMvcApp06SendingDataToJspFromControllerUsingModelApplication.class, args);
+    }
+}
