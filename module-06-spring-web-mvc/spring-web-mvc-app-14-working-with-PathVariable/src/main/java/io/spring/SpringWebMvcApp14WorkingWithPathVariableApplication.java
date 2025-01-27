88d33c7ac08363b4cbc885109c33a86ac42f8c99
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-01-27 21:58:15 +0530

    working with @PathVariable

---
+++ module-06-spring-web-mvc/spring-web-mvc-app-14-working-with-PathVariable/src/main/java/io/spring/SpringWebMvcApp14WorkingWithPathVariableApplication.java
@@ -0,0 +1,11 @@
+package io.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebMvcApp14WorkingWithPathVariableApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebMvcApp14WorkingWithPathVariableApplication.class, args);
+    }
+}
