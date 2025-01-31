c3e04ff9288f982932c9c9d3bb7b07f055c8b9ed
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-01-31 21:16:49 +0530

    message expression

---
+++ module-06-spring-web-mvc/spring-web-mvc-app-18-using-thymeleaf-message-expression/src/main/java/io/spring/SpringWebMvcApp18UsingThymeleafMessageExpressionApplication.java
@@ -0,0 +1,11 @@
+package io.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebMvcApp18UsingThymeleafMessageExpressionApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebMvcApp18UsingThymeleafMessageExpressionApplication.class, args);
+    }
+}
