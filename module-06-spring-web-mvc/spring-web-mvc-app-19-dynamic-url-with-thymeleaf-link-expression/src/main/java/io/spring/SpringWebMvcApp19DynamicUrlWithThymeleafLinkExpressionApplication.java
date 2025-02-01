63e42094a47540d99d52c422220659e3b5e61c2b
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-02-01 19:30:46 +0530

    working with link expression

--- module-06-spring-web-mvc/spring-web-mvc-app-19-dynamic-url-with-thymeleaf-link-expression/src/main/java/io/spring/SpringWebMvcApp19DynamicUrlWithThymeleafLinkExpressionApplication.java
+++ module-06-spring-web-mvc/spring-web-mvc-app-19-dynamic-url-with-thymeleaf-link-expression/src/main/java/io/spring/SpringWebMvcApp19DynamicUrlWithThymeleafLinkExpressionApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringWebMvcApp19DynamicUrlWithThymeleafLinkExpressionApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SpringWebMvcApp19DynamicUrlWithThymeleafLinkExpressionApplication.class, args);
     }
-
 }
