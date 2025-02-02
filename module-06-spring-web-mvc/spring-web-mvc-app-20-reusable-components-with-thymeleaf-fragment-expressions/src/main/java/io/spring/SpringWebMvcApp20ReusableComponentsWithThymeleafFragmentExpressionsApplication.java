90ff69503b113a9eb3b993ba2444b34cb8dfff8b
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-02-02 20:22:38 +0530

    fragment expression

---
+++ module-06-spring-web-mvc/spring-web-mvc-app-20-reusable-components-with-thymeleaf-fragment-expressions/src/main/java/io/spring/SpringWebMvcApp20ReusableComponentsWithThymeleafFragmentExpressionsApplication.java
@@ -0,0 +1,11 @@
+package io.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebMvcApp20ReusableComponentsWithThymeleafFragmentExpressionsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebMvcApp20ReusableComponentsWithThymeleafFragmentExpressionsApplication.class, args);
+    }
+}
