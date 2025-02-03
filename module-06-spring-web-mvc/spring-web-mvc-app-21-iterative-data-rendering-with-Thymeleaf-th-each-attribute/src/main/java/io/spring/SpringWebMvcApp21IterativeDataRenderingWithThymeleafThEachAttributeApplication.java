c1fbf14834792279a46b9609baf3927cdc862c38
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-02-03 09:52:20 +0530

    iterative data rendering with thymeleaf th:each attribute

---
+++ module-06-spring-web-mvc/spring-web-mvc-app-21-iterative-data-rendering-with-Thymeleaf-th-each-attribute/src/main/java/io/spring/SpringWebMvcApp21IterativeDataRenderingWithThymeleafThEachAttributeApplication.java
@@ -0,0 +1,11 @@
+package io.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebMvcApp21IterativeDataRenderingWithThymeleafThEachAttributeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebMvcApp21IterativeDataRenderingWithThymeleafThEachAttributeApplication.class, args);
+    }
+}
