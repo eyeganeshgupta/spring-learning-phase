05dcb309945e48095ee909deb5d9b33692762261
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-02-06 21:30:00 +0530

    spring rest development environment setup

---
+++ module-07-spring-rest/spring-rest-app-01/src/main/java/io/spring/SpringRestApp01Application.java
@@ -0,0 +1,13 @@
+package io.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestApp01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestApp01Application.class, args);
+    }
+
+}
