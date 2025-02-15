7e30622b7d4e299babc1f107f6e268ed5fe9bbb0
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-02-15 18:48:52 +0530

    working with payloads

---
+++ module-07-spring-rest/spring-rest-app-09-handling-json-payloads-with-RequestBody/src/main/java/io/spring/response/ApiResponse.java
@@ -0,0 +1,31 @@
+package io.spring.response;
+
+public class ApiResponse<T> {
+    private boolean success;
+    private String message;
+    private T data;
+
+    public boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
