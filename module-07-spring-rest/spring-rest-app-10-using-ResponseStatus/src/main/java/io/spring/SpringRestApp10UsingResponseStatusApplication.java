200da34a8a5a525b719af118509a3a0cac2651c0
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-04-24 21:02:15 +0530

    model, service and response

---
+++ module-07-spring-rest/spring-rest-app-10-using-ResponseStatus/src/main/java/io/spring/SpringRestApp10UsingResponseStatusApplication.java
@@ -0,0 +1,11 @@
+package io.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestApp10UsingResponseStatusApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestApp10UsingResponseStatusApplication.class, args);
+	}
+}
