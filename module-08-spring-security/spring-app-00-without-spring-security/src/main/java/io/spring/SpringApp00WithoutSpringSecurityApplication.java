0a249dfa4eef700485a27b3eef8ec927068f21c4
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-04-30 23:26:54 +0530

    spring app without spring security

---
+++ module-08-spring-security/spring-app-00-without-spring-security/src/main/java/io/spring/SpringApp00WithoutSpringSecurityApplication.java
@@ -0,0 +1,11 @@
+package io.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApp00WithoutSpringSecurityApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApp00WithoutSpringSecurityApplication.class, args);
+    }
+}
