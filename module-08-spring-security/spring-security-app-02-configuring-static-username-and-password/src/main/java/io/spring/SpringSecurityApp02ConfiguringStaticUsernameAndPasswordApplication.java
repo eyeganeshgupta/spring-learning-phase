697089212250b949d19ef44cd09f1baa0dd10b9f
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-05-02 23:03:48 +0530

    spring security setup

---
+++ module-08-spring-security/spring-security-app-02-configuring-static-username-and-password/src/main/java/io/spring/SpringSecurityApp02ConfiguringStaticUsernameAndPasswordApplication.java
@@ -0,0 +1,11 @@
+package io.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityApp02ConfiguringStaticUsernameAndPasswordApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityApp02ConfiguringStaticUsernameAndPasswordApplication.class, args);
+    }
+}
