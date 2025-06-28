e6e73647f8a4df26a59f2936bffd2df03c3ddd53
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-06-28 16:41:03 +0530

    application configuration

---
+++ module-08-spring-security/spring-security-app-03-modifying-default-security-configuration/src/main/java/io/spring/SpringSecurityApp03ModifyingDefaultSecurityConfigurationApplication.java
@@ -0,0 +1,11 @@
+package io.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityApp03ModifyingDefaultSecurityConfigurationApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityApp03ModifyingDefaultSecurityConfigurationApplication.class, args);
+	}
+}
