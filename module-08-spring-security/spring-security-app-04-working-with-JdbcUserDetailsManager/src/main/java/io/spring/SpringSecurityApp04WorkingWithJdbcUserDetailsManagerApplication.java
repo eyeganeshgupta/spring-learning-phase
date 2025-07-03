b9efaa0e2319f5058cc131528cbb88b133a80b3c
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-07-03 20:22:11 +0530

    application configuration

---
+++ module-08-spring-security/spring-security-app-04-working-with-JdbcUserDetailsManager/src/main/java/io/spring/SpringSecurityApp04WorkingWithJdbcUserDetailsManagerApplication.java
@@ -0,0 +1,11 @@
+package io.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityApp04WorkingWithJdbcUserDetailsManagerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityApp04WorkingWithJdbcUserDetailsManagerApplication.class, args);
+	}
+}
