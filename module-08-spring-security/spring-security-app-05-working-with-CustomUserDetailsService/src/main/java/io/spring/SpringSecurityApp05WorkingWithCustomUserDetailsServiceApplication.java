ef36eddb04e3d548c48843d099ce27b75c29e5fe
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-07-06 19:28:53 +0530

    application configuration

---
+++ module-08-spring-security/spring-security-app-05-working-with-CustomUserDetailsService/src/main/java/io/spring/SpringSecurityApp05WorkingWithCustomUserDetailsServiceApplication.java
@@ -0,0 +1,11 @@
+package io.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityApp05WorkingWithCustomUserDetailsServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityApp05WorkingWithCustomUserDetailsServiceApplication.class, args);
+    }
+}
