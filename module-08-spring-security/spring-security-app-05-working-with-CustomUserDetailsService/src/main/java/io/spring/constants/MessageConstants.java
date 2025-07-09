3462d12514eff6196f0a8f3226a42fee19159525
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-07-09 21:03:07 +0530

    customer service

---
+++ module-08-spring-security/spring-security-app-05-working-with-CustomUserDetailsService/src/main/java/io/spring/constants/MessageConstants.java
@@ -0,0 +1,8 @@
+package io.spring.constants;
+
+public class MessageConstants {
+
+    public static final String REGISTRATION_SUCCESS = "Customer registered successfully";
+    public static final String EMAIL_ALREADY_EXISTS = "Email already exists";
+
+}
---
+++ module-08-spring-security/spring-security-app-05-working-with-CustomUserDetailsService/src/main/java/io/spring/service/CustomerService.java
@@ -0,0 +1,36 @@
+package io.spring.service;
+
+import io.spring.constants.MessageConstants;
+import io.spring.dto.CustomerDTO;
+import io.spring.model.Customer;
+import io.spring.repository.CustomerRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+
+    private final CustomerRepository customerRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public CustomerService(CustomerRepository customerRepository, PasswordEncoder passwordEncoder) {
+        this.customerRepository = customerRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public String registerCustomer(CustomerDTO customerDTO) {
+        if (customerRepository.existsByEmail(customerDTO.getEmail())) {
+            return MessageConstants.EMAIL_ALREADY_EXISTS;
+        }
+
+        Customer customer = new Customer();
+        customer.setEmail(customerDTO.getEmail());
+        customer.setPassword(passwordEncoder.encode(customerDTO.getPassword()));
+        customer.setRole(customerDTO.getRole());
+
+        customerRepository.save(customer);
+
+        return MessageConstants.REGISTRATION_SUCCESS;
+    }
+
+}
