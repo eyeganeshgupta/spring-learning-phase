e71654bc3d42edbba48293c0bbd10e80ff366b45
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-07-13 19:43:52 +0530

    application configuration

---
+++ module-08-spring-security/spring-security-app-06-DelegatingPasswordEncoder/src/main/java/io/spring/SpringSecurityApp06DelegatingPasswordEncoderApplication.java
@@ -0,0 +1,11 @@
+package io.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityApp06DelegatingPasswordEncoderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityApp06DelegatingPasswordEncoderApplication.class, args);
+    }
+}
