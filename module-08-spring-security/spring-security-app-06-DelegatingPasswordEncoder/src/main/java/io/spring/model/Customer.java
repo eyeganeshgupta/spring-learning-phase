7eb7deaad9417550ed5006ab9a1e92d5cbf2c4f2
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-07-13 19:46:26 +0530

    customer entity

---
+++ module-08-spring-security/spring-security-app-06-DelegatingPasswordEncoder/src/main/java/io/spring/model/Customer.java
@@ -0,0 +1,64 @@
+package io.spring.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "customer")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 255)
+    private String email;
+
+    @Column(nullable = false, length = 255)
+    private String password;
+
+    @Column(nullable = false, length = 30)
+    private String role;
+
+    public Customer() {
+
+    }
+
+    public Customer(String email, String password, String role) {
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+}
