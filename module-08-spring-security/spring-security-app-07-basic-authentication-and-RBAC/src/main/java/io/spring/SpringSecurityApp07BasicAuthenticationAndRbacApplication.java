289cc615310a72f6820baf560ef844a500b09e0e
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-07-21 21:22:49 +0530

    application configuration

---
+++ module-08-spring-security/spring-security-app-07-basic-authentication-and-RBAC/src/main/java/io/spring/SpringSecurityApp07BasicAuthenticationAndRbacApplication.java
@@ -0,0 +1,11 @@
+package io.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityApp07BasicAuthenticationAndRbacApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityApp07BasicAuthenticationAndRbacApplication.class, args);
+    }
+}
