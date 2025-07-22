510cff65af2b8a57cf9a2158d23179f90a4c7c8e
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-07-22 21:43:10 +0530

    user entity

---
+++ module-08-spring-security/spring-security-app-07-basic-authentication-and-RBAC/src/main/java/io/spring/entity/User.java
@@ -0,0 +1,31 @@
+package io.spring.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Set<Role> roles;
+
+    public User(String username, String password, Set<Role> roles) {
+        this.username = username;
+        this.password = password;
+        this.roles = roles;
+    }
+
+}
