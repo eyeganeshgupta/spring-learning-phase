c2898f2423be425ffb0ae71dae9361e87f49bdd6
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-07-23 09:08:17 +0530

    product entity

---
+++ module-08-spring-security/spring-security-app-07-basic-authentication-and-RBAC/src/main/java/io/spring/entity/Product.java
@@ -0,0 +1,31 @@
+package io.spring.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+    private double price;
+
+    @ManyToOne
+    private Category category;
+
+    public Product(String name, String description, double price, Category category) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.category = category;
+    }
+
+}
