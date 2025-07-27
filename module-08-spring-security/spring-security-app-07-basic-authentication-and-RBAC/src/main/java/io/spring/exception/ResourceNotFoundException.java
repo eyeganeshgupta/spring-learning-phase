629c9a0127c342e2a916d64ceb5f50c06ad31934
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-07-27 09:32:50 +0530

    resource not found exception

---
+++ module-08-spring-security/spring-security-app-07-basic-authentication-and-RBAC/src/main/java/io/spring/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package io.spring.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
