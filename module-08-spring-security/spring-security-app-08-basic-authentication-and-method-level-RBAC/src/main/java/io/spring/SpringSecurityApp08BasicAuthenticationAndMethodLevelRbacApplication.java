28aecb635c54535a9e36432eb6fcc9583fd24630
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-08-02 19:57:42 +0530

    application configuration

---
+++ module-08-spring-security/spring-security-app-08-basic-authentication-and-method-level-RBAC/src/main/java/io/spring/SpringSecurityApp08BasicAuthenticationAndMethodLevelRbacApplication.java
@@ -0,0 +1,11 @@
+package io.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityApp08BasicAuthenticationAndMethodLevelRbacApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityApp08BasicAuthenticationAndMethodLevelRbacApplication.class, args);
+    }
+}
