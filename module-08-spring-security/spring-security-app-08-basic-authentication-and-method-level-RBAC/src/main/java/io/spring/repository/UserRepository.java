c3de839a6a315417b3d8302307c65015b29d141f
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-08-03 20:28:33 +0530

    user repository

---
+++ module-08-spring-security/spring-security-app-08-basic-authentication-and-method-level-RBAC/src/main/java/io/spring/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package io.spring.repository;
+
+import io.spring.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
