65acda62beb651a03dac2f69ba46addc40b278b5
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-08-06 20:16:47 +0530

    custom access denied handler

---
+++ module-08-spring-security/spring-security-app-08-basic-authentication-and-method-level-RBAC/src/main/java/io/spring/filter/CustomAccessDeniedHandler.java
@@ -0,0 +1,45 @@
+package io.spring.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.spring.response.ApiResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(CustomAccessDeniedHandler.class);
+    private final ObjectMapper mapper;
+
+    public CustomAccessDeniedHandler(ObjectMapper mapper) {
+        this.mapper = mapper;
+    }
+
+    @Override
+    public void handle(HttpServletRequest request,
+                       HttpServletResponse response,
+                       AccessDeniedException accessDeniedException) throws IOException, ServletException {
+
+        logger.info("Access denied for URI: {}", request.getRequestURI());
+        logger.info("AccessDeniedException message: {}", accessDeniedException.getMessage());
+
+        ApiResponse<Object> errorResponse = ApiResponse.builder()
+                .success(false)
+                .message(accessDeniedException.getMessage())
+                .data(accessDeniedException.getMessage())
+                .build();
+
+        response.setContentType("application/json");
+        response.setStatus(HttpStatus.FORBIDDEN.value());
+        response.getWriter().write(mapper.writeValueAsString(errorResponse));
+    }
+}
