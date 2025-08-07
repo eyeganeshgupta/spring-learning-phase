7bfa9e728bf8bf8b323bc0ebea1f0d49a319adf7
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-08-07 20:40:20 +0530

    custom authentication entry point

---
+++ module-08-spring-security/spring-security-app-08-basic-authentication-and-method-level-RBAC/src/main/java/io/spring/filter/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,86 @@
+package io.spring.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.spring.response.ApiResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.LockedException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private static final Logger logger = LoggerFactory.getLogger(CustomAuthenticationEntryPoint.class);
+    private final ObjectMapper objectMapper;
+
+    public CustomAuthenticationEntryPoint(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    /*
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) throws IOException, ServletException {
+
+        String errorMessage = "Invalid username or password";
+
+        if (authException instanceof LockedException) {
+            errorMessage = "Your account has been locked due to too many failed login attempts. Please contact support or try again later.";
+        }
+
+        logger.info("Unauthorized access attempt to URI: {}", request.getRequestURI());
+        logger.info("AuthenticationException message: {}", errorMessage);
+
+        ApiResponse<Object> errorResponse = ApiResponse.builder()
+                .success(false)
+                .message(errorMessage)
+                .data(errorMessage)
+                .build();
+
+        response.setContentType("application/json");
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
+    }
+     */
+
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) throws IOException, ServletException {
+
+        String errorMessage = "Invalid username or password";
+
+        Throwable cause = authException;
+        while (cause != null) {
+            if (cause instanceof LockedException) {
+                errorMessage = "Your account has been locked due to too many failed login attempts. Please contact support or try again later.";
+                break;
+            }
+            cause = cause.getCause();
+        }
+
+        logger.info("Unauthorized access attempt to URI: {}", request.getRequestURI());
+        logger.info("AuthenticationException message: {}", errorMessage);
+
+        ApiResponse<Object> errorResponse = ApiResponse.builder()
+                .success(false)
+                .message(errorMessage)
+                .data(errorMessage)
+                .build();
+
+        response.reset();
+
+        response.setContentType("application/json");
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
+    }
+}
