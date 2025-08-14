477529bd387222d17c3a5aa7f7d4f7305a814e9b
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-08-14 22:43:31 +0530

    custom user details service

---
+++ module-08-spring-security/spring-security-app-08-basic-authentication-and-method-level-RBAC/src/main/java/io/spring/security/CustomUserDetailsService.java
@@ -0,0 +1,34 @@
+package io.spring.security;
+
+import io.spring.model.User;
+import io.spring.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    private static final Logger logger = LoggerFactory.getLogger(CustomUserDetailsService.class);
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        logger.info("Attempting to load user by username: {}", username);
+
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            logger.info("User not found: {}", username);
+            throw new UsernameNotFoundException("User not found: " + username);
+        }
+        logger.info("User found: {}", username);
+        return user;
+    }
+}
