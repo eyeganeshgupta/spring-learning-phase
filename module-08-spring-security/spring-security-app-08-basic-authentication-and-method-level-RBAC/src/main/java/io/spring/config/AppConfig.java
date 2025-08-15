44b779736e537d1e678862adcda7af5027ae3846
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-08-15 08:36:23 +0530

    application configuration

---
+++ module-08-spring-security/spring-security-app-08-basic-authentication-and-method-level-RBAC/src/main/java/io/spring/config/AppConfig.java
@@ -0,0 +1,18 @@
+package io.spring.config;
+
+import io.spring.filter.RateLimitFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public FilterRegistrationBean<RateLimitFilter> rateLimitFilter() {
+        FilterRegistrationBean<RateLimitFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new RateLimitFilter());
+        registrationBean.addUrlPatterns("/auth/*", "/documents/*");
+        return registrationBean;
+    }
+}
