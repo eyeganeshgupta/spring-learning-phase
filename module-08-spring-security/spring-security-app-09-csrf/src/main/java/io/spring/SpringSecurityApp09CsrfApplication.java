1f243769f4426944f8b7e2fd07e727dfba0c769a
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-08-17 09:59:30 +0530

    configuration

---
+++ module-08-spring-security/spring-security-app-09-csrf/src/main/java/io/spring/SpringSecurityApp09CsrfApplication.java
@@ -0,0 +1,13 @@
+package io.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityApp09CsrfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityApp09CsrfApplication.class, args);
+    }
+
+}
