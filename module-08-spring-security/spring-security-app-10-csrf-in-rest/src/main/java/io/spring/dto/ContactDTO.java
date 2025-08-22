4de5a7ac50ed3bd8dae98e039ad41a44d6b42ad3
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-08-22 09:29:26 +0530

    contact dto

---
+++ module-08-spring-security/spring-security-app-10-csrf-in-rest/src/main/java/io/spring/dto/ContactDTO.java
@@ -0,0 +1,76 @@
+package io.spring.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class ContactDTO {
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @Size(max = 15, message = "Phone number should be 15 digits max")
+    private String phone;
+
+    @NotBlank(message = "Subject is required")
+    private String subject;
+
+    @NotBlank(message = "Message is required")
+    private String message;
+
+    public ContactDTO() {
+
+    }
+
+    public ContactDTO(String name, String email, String phone, String subject, String message) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.subject = subject;
+        this.message = message;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
