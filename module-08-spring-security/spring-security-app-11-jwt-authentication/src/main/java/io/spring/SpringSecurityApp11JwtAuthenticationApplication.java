45c213ed643706edecf35bba2b21b22ec0584607
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-08-25 09:22:30 +0530

    jwt setup

---
+++ module-08-spring-security/spring-security-app-11-jwt-authentication/src/main/java/io/spring/SpringSecurityApp11JwtAuthenticationApplication.java
@@ -0,0 +1,11 @@
+package io.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityApp11JwtAuthenticationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityApp11JwtAuthenticationApplication.class, args);
+    }
+}
