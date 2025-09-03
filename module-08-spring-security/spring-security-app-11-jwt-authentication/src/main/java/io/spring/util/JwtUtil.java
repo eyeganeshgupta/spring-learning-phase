102b742efa2016859202bf27238f714cc0e8a9d2
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-09-03 09:28:00 +0530

    jwt secret key

---
+++ module-08-spring-security/spring-security-app-11-jwt-authentication/src/main/java/io/spring/util/JwtUtil.java
@@ -0,0 +1,12 @@
+package io.spring.util;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtUtil {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+}
