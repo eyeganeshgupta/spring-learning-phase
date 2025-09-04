2cd7277806e1805488580536a1098f4d30cdb8e2
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-09-04 09:19:06 +0530

    getting signing key

--- module-08-spring-security/spring-security-app-11-jwt-authentication/src/main/java/io/spring/util/JwtUtil.java
+++ module-08-spring-security/spring-security-app-11-jwt-authentication/src/main/java/io/spring/util/JwtUtil.java
@@ -3,10 +3,23 @@ package io.spring.util;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
+
+import io.jsonwebtoken.security.Keys;
+
 @Component
 public class JwtUtil {
 
     @Value("${jwt.secret}")
     private String secretKey;
+
+    public JwtUtil() {
+        System.out.println("secretKey: " + secretKey);
+    }
+
+    private SecretKey getSigningKey() {
+        SecretKey key = Keys.hmacShaKeyFor(secretKey.getBytes());
+        return key;
+    }
 
 }
