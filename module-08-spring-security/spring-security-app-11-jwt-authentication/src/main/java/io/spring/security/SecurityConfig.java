c83342d26475522211e98eef2cf3b13dd699224a
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-09-13 21:06:28 +0530

    configuring spring security

--- module-08-spring-security/spring-security-app-11-jwt-authentication/src/main/java/io/spring/security/SecurityConfig.java
+++ module-08-spring-security/spring-security-app-11-jwt-authentication/src/main/java/io/spring/security/SecurityConfig.java
@@ -3,8 +3,10 @@ package io.spring.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
@@ -23,5 +25,11 @@ public class SecurityConfig {
         ).sessionManagement(session ->
                 session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         );
+
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        return http.build();
     }
 }
