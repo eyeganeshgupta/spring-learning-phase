6ba13e1bb69969ec706120948f6e8bed2e96e7e2
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-09-14 19:37:33 +0530

    configuring authentication manager

--- module-08-spring-security/spring-security-app-11-jwt-authentication/src/main/java/io/spring/security/SecurityConfig.java
+++ module-08-spring-security/spring-security-app-11-jwt-authentication/src/main/java/io/spring/security/SecurityConfig.java
@@ -2,6 +2,8 @@ package io.spring.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -38,6 +40,13 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authenticationConfiguration
+    ) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
 }
