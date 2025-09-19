54d2d5dad666678c1bf9a0887179c6c48ab51300
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-09-19 23:30:11 +0530

    auth controller

---
+++ module-08-spring-security/spring-security-app-11-jwt-authentication/src/main/java/io/spring/controller/AuthController.java
@@ -0,0 +1,20 @@
+package io.spring.controller;
+
+import io.spring.service.AuthService;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private AuthService authService;
+    private AuthenticationManager authenticationManager;
+
+    public AuthController(AuthService authService, AuthenticationManager authenticationManager) {
+        this.authService = authService;
+        this.authenticationManager = authenticationManager;
+    }
+
+}
