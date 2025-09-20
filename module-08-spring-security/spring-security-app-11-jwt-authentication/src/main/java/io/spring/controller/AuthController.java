e149e9a17a53e755c66893944e46e4c6f232876f
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-09-20 23:25:11 +0530

    register post mapping

--- module-08-spring-security/spring-security-app-11-jwt-authentication/src/main/java/io/spring/controller/AuthController.java
+++ module-08-spring-security/spring-security-app-11-jwt-authentication/src/main/java/io/spring/controller/AuthController.java
@@ -1,7 +1,12 @@
 package io.spring.controller;
 
+import io.spring.dto.RegisterRequest;
 import io.spring.service.AuthService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +20,12 @@ public class AuthController {
     public AuthController(AuthService authService, AuthenticationManager authenticationManager) {
         this.authService = authService;
         this.authenticationManager = authenticationManager;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@Valid @RequestBody RegisterRequest registerRequest) {
+        authService.register(registerRequest.getEmail(), registerRequest.getPassword(), registerRequest.getBalance());
+        return ResponseEntity.ok("Successfully registered");
     }
 
 }
