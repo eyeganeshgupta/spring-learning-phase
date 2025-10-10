08246cbb3d05c5c073e271a1dae7f624499cc928
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-10-10 08:16:38 +0530

    jwt setup

---
+++ module-08-spring-security/spring-security-app-12-jwt-rbac-authentication/src/main/java/io/spring/SpringSecurityApp12JwtRbacAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package io.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityApp12JwtRbacAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityApp12JwtRbacAuthenticationApplication.class, args);
+    }
+
+}
