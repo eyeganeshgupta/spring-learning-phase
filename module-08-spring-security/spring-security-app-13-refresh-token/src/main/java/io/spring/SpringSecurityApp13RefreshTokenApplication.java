39f797a36afceb2841a53637460967c6b8675ac6
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-11-09 22:37:45 +0530

    project configuration

---
+++ module-08-spring-security/spring-security-app-13-refresh-token/src/main/java/io/spring/SpringSecurityApp13RefreshTokenApplication.java
@@ -0,0 +1,11 @@
+package io.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityApp13RefreshTokenApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityApp13RefreshTokenApplication.class, args);
+    }
+}
