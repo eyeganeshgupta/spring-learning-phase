255a97dd431722448b45735b591837428a2b3fb1
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-11-11 23:15:23 +0530

    api error dto

---
+++ module-08-spring-security/spring-security-app-13-refresh-token/src/main/java/io/spring/dto/ApiError.java
@@ -0,0 +1,53 @@
+package io.spring.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.List;
+
+public class ApiError {
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private final LocalDateTime timestamp = LocalDateTime.now();
+    private final int status;
+    private final String error;
+    private final String message;
+    private final String path;
+    private final List<String> details;
+
+    public ApiError(int status, String error, String message, String path, List<String> details) {
+        this.status = status;
+        this.error = error;
+        this.message = message;
+        this.path = path;
+        this.details = details == null ? Collections.emptyList() : details;
+    }
+
+    public ApiError(int status, String error, String message, String path) {
+        this(status, error, message, path, Collections.emptyList());
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public List<String> getDetails() {
+        return details;
+    }
+}
