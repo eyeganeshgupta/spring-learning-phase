4b153346fd6d8c6ef6dbbbbf9dcf92b6668d8d31
eyeganeshgupta <eyeganeshgupta@gmail.com>
2025-11-22 22:39:17 +0530

    configuring BCryptPasswordEncoder

--- module-08-spring-security/spring-security-app-13-refresh-token/src/main/java/io/spring/security/SecurityConfig.java
+++ module-08-spring-security/spring-security-app-13-refresh-token/src/main/java/io/spring/security/SecurityConfig.java
@@ -4,9 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -54,6 +58,12 @@ public class SecurityConfig {
 
         logger.info("SecurityFilterChain initialized successfully.");
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        logger.info("Initializing BCryptPasswordEncoder...");
+        return new BCryptPasswordEncoder();
     }
 
 }
